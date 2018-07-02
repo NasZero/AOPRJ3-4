c716427e01cea1ded8a672a5361d511ebcbfe1a6
Lars <larsvandekeuken@live.nl>
2018-07-02 21:38:37 +0200

    Update

---
+++ AMO03/Taak 3/Site/index.php
@@ -0,0 +1,64 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: Lars
+ * Date: 20-6-2018
+ * Time: 14:36
+ */
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Login Of Registreer</title>
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+
+<body>
+        <div class="login-box">
+            <div class="inner-box">
+                <div class="box-space">
+                        <div class="box-top">
+                            <center>
+                                <h1>MA TWENTE</h1>
+                                <div class="border"></div>
+                            </center>
+                        </div>
+                    <div class="form">
+                        <center>
+                            <form action="/action_page.php">
+                                <input type="text" placeholder="Voornaam" name="username">
+                                <input type="text" placeholder="Tussenvoegsel" name="username">
+                                <br>
+                                <br>
+                                <br>
+                                <input type="password" placeholder="Achternaam" name="password">
+                                <input type="text" placeholder="Gebruikersnaam" name="username">
+                                <br>
+                                <br>
+                                <br>
+                                <input type="password" placeholder="Wachtwoord" name="password">
+                                <input type="text" placeholder="Wachtwoord herhalen" name="username">
+                                <br>
+                                <br>
+                                <br>
+                                <input style="width: 76.5%" type="text" placeholder="Sleutel" name="username">
+                                <br>
+                                <br>
+                                <br>
+                                <input type="submit" value="Registreren">
+                            </form>
+                        </center>
+                    </div>
+                    <br>
+                    <br>
+                    <br>
+                    <br>
+                    <br>
+                    <center><p class="footer">Lars van de Keuken, Aron huis in't Veld</p></center>
+                </div>
+            </div>
+        </div>
+</body>
+
+</html>
